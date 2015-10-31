0e86e647add5b6489caf0a25eee64bbb2bc4b0ec
Fred Criscuolo <fjcriscuolo@ucsd.edu>
2015-10-31 11:00:10 -0400

    new gene alteration service

--- src/main/java/org/mskcc/cbio/rxjava/jdbc/DataServices.java
+++ src/main/java/org/mskcc/cbio/rxjava/jdbc/DataServices.java
@@ -3,30 +3,30 @@ package org.mskcc.cbio.rxjava.jdbc;
 
         import java.sql.Connection;
         import java.sql.ResultSet;
-        import java.sql.SQLException;
-        import java.sql.Statement;
         import java.util.ArrayList;
         import java.util.List;
+        import java.util.Set;
         import java.util.concurrent.CountDownLatch;
-
-        import com.google.common.base.Optional;
+        import java.util.concurrent.ExecutorService;
+        import java.util.concurrent.Executors;
+        import java.util.concurrent.TimeUnit;
+        import com.google.common.base.Joiner;
+        import com.google.common.base.Splitter;
+        import com.google.common.base.Stopwatch;
+        import com.google.common.collect.Sets;
+        import com.sun.istack.internal.Nullable;
         import org.apache.log4j.Logger;
         import org.mskcc.cbio.rxjava.jdbc.model.Cbio;
-        import rx.Observable;
-
-        import rx.Observer;
-        import rx.Subscriber;
-        import rx.Subscription;
-        import rx.functions.Action1;
-        import rx.observables.AbstractOnSubscribe;
+        import rx.*;
+        import rx.functions.Action0;
         import rx.schedulers.Schedulers;
 
 public class DataServices {
 
     private static final Logger logger = Logger.getLogger(DataServices.class);
+    private static final Splitter commaSplitter = Splitter.on(',');
+    private static final Joiner commaJoiner = Joiner.on(",");
     public static List<Cbio.CancerStudy> getCancerStudyList() {
-        //Optional<Connection> connOpt = ConnectionService.INSTANCE.getCbioConnection();
-
         try (Connection c = ConnectionService.INSTANCE.getCbioConnection().get()) {
             ResultSet rs = c.createStatement().executeQuery("select * from cgds_gdac.cancer_study");
             ArrayList<Cbio.CancerStudy> genes = new ArrayList<>();
@@ -63,22 +63,95 @@ public class DataServices {
             }
         }).subscribeOn(Schedulers.io());
     }
+
+
+    public  static Observable<Object> getGeneticAlterationObs(Integer geneticProfileId, @Nullable Set<Long> entrezIdSet) {
+        Connection c = ConnectionService.INSTANCE.getCbioConnection().get();
+         String queryString = "SELECT * FROM genetic_alteration WHERE "
+                + " GENETIC_PROFILE_ID = " + geneticProfileId;
+        if( entrezIdSet != null && entrezIdSet.size()>0 ) {
+            queryString = queryString + " AND ENTREZ_GENE_ID IN ("
+                    + commaJoiner.join(entrezIdSet) + ")";
+        }
+
+        final String query = queryString;
+        return Observable.create(o -> {
+            try (ResultSet rs = c.createStatement().executeQuery( query)) {
+                while (rs.next()) {
+                    o.onNext(Cbio.GeneticAlteration.create(rs.getInt("GENETIC_PROFILE_ID"), rs.getInt("ENTREZ_GENE_ID"),
+                           commaSplitter.splitToList(rs.getString("VALUES"))));
+                }
+                logger.info("Query completed");
+
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                o.onError(e);
+            }
+           // o.onCompleted();
+        }).subscribeOn(Schedulers.immediate()).take(500).onBackpressureBuffer();
+    }
+
     public static void main(String...args) {
         final CountDownLatch latch = new CountDownLatch(1);
-        Subscription csSub = getCancerStudyAsync().subscribe(new Observer<Cbio.CancerStudy>() {
+
+        Set<Long> geneSet = Sets.newHashSet();
+        geneSet.add(677835L);
+        geneSet.add(9636L);
+        geneSet.add(728661L);
+        geneSet.add(118424L);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+       //Observable<Object> geneticAltObs = DataServices.getGeneticAlterationObs(2,geneSet);
+         Observable<Object> geneticAltObs = DataServices.getGeneticAlterationObs(2,null);
+        final Stopwatch subWatch = Stopwatch.createUnstarted();
+        geneticAltObs.subscribe(new Subscriber<Object>() {
+            @Override
+            public void onStart(){
+                subWatch.start();
+                logger.info("Subscriber starting");
+                //request(100L);
+            }
+
             @Override
             public void onCompleted() {
-                latch.countDown(); // signal completion
+                subWatch.stop();
+                long seconds = subWatch.elapsed(TimeUnit.SECONDS);
+                logger.info("Subscription completed  time = " +seconds +" seconds");
+                latch.countDown();
+
             }
+
             @Override
             public void onError(Throwable throwable) {
                 logger.error(throwable.getMessage());
+                subWatch.stop();
+                latch.countDown();
             }
+
             @Override
-            public void onNext(Cbio.CancerStudy cancerStudy) {
-                logger.info(cancerStudy.toString());
+            public void onNext(Object o) {
+                Scheduler.Worker worker = Schedulers.from(executor).createWorker();
+                final Subscription schedule = worker.schedule(new Action0() {
+                    @Override
+                    public void call() {
+                        logger.info(Thread.currentThread().getName()); // display what thread this is running on
+                        logger.info(o.toString());
+                        try {
+                            // complete a compute intensive task here
+                            Thread.sleep(500L);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        //request(100L);
+                    }
+
+                });
+
             }
         });
+
+
+
         try {
             latch.await();
             logger.info("FINIS....");
@@ -86,4 +159,6 @@ public class DataServices {
             e.printStackTrace();
         }
     }
+
+
 }
